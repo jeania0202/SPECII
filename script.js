c00fb1c1539ed1ff908685be9e3d5aa426d16625
Rhea Cabael <jeania0202@gmail.com>
2026-05-14 20:27:50 +0800

    some changes on js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playerChoiceDisplay = document.getElementById("player-Choice");
 const computerChoiceDisplay = document.getElementById("computer-Choice");
 const resultText = document.getElementById("result-text");
 
-const choices = ["rock", "paper", "scissors"];
+const choices = ["ROCK", "PAPER", "SCISSORS"];
 const rock=document.getElementById("rock");
 const paper=document.getElementById("paper");
 const scissors=document.getElementById("scissor");
@@ -11,9 +11,9 @@ const computerChoice = choices[Math.floor(Math.random() * 3)];
 function playgame(player){
     if(player === computerChoice){
         resultText.textContent = "It's a tie!";
-    } else if((player === "rock" && computerChoice === "scissors") ||
-              (player === "paper" && computerChoice === "rock") ||
-              (player === "scissors" && computerChoice === "paper")){
+    } else if((player === "ROCK" && computerChoice === "SCISSORS") ||
+              (player === "PAPER" && computerChoice === "ROCK") ||
+              (player === "SCISSORS" && computerChoice === "PAPER")){
         resultText.textContent = "You win!";
     } else {
         resultText.textContent = "Computer wins!";
@@ -21,7 +21,7 @@ function playgame(player){
     playerChoiceDisplay.textContent = `YOU: ${player}`;
     computerChoiceDisplay.textContent = `COMPUTER: ${computerChoice}`;
 }
-rock.addEventListener("click", () => playgame("rock"));
-paper.addEventListener("click", () => playgame("paper"));
-scissors.addEventListener("click", () => playgame("scissors"));
+rock.addEventListener("click", () => playgame("ROCK"));
+paper.addEventListener("click", () => playgame("PAPER"));
+scissors.addEventListener("click", () => playgame("SCISSORS"));
 
