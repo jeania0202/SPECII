2587e94c9c63568ec292aa58ccf58513c11dc5c4
Rhea Cabael <jeania0202@gmail.com>
2026-05-16 20:25:41 +0800

    changed made in the script

--- script.js
+++ script.js
@@ -2,26 +2,78 @@ const playerChoiceDisplay = document.getElementById("player-Choice");
 const computerChoiceDisplay = document.getElementById("computer-Choice");
 const resultText = document.getElementById("result-text");
 
+const playerScoreText = document.getElementById("player-score");
+const computerScoreText = document.getElementById("computer-score");
+const finalResult = document.getElementById("final-result");
+
+const resetBtn = document.getElementById("reset-btn");
+const gameBox = document.querySelector(".game-container");
+
 const choices = ["ROCK", "PAPER", "SCISSORS"];
+
 const rock=document.getElementById("rock");
 const paper=document.getElementById("paper");
 const scissors=document.getElementById("scissor");
 const computerChoice = choices[Math.floor(Math.random() * 3)];
 
+let playerScore = 0;
+let computerScore = 0;
+let gameOver = false;
+
 function playgame(player){
     if(player === computerChoice){
         resultText.textContent = "It's a tie!";
-    } else if((player === "ROCK" && computerChoice === "SCISSORS") ||
-              (player === "PAPER" && computerChoice === "ROCK") ||
-              (player === "SCISSORS" && computerChoice === "PAPER")){
-        resultText.textContent = "You win!";
+        return;
+    } 
+    if(
+        (player === "ROCK" && computerChoice === "SCISSORS") ||
+        (player === "PAPER" && computerChoice === "ROCK") ||
+        (player === "SCISSORS" && computerChoice === "PAPER"))
+    {
+        playerScore++;
+        resultText.textContent = "YOU WIN! 🔥";
+        gameBox.classList.add("shake");
+        
     } else {
-        resultText.textContent = "Computer wins!";
+        computerScore++;
+        resultText.textContent = "COMPUTER WINS! 💻";
+        gameBox.classList.add("shake");
     }
+
+        setTimeout(() => gameBox.classList.remove("shake"), 300);
+
     playerChoiceDisplay.textContent = `YOU: ${player}`;
     computerChoiceDisplay.textContent = `COMPUTER: ${computerChoice}`;
 }
+
+function checkWinner() {
+    if (playerScore === 10 || computerScore === 10) {
+        gameOver = true;
+
+        if (playerScore === 10) {
+            finalResult.textContent = "🏆 CONGRATULATIONS YOU WIN!";
+        } else {
+            finalResult.textContent = "💀 COMPUTER WINS!";
+        }
+    }
+}
+
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    gameOver = false;
+
+    playerScoreText.textContent = "YOU: 0";
+    computerScoreText.textContent = "COMPUTER: 0";
+
+    playerChoiceDisplay.textContent = "YOU:";
+    computerChoiceDisplay.textContent = "COMPUTER:";
+    resultText.textContent = "RESULT:";
+    finalResult.textContent = "";
+}
+
 rock.addEventListener("click", () => playgame("ROCK"));
 paper.addEventListener("click", () => playgame("PAPER"));
 scissors.addEventListener("click", () => playgame("SCISSORS"));
-
+resetBtn.addEventListener("click", resetGame);
